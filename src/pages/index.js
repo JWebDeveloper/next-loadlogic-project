e757cadb09378da01cf60a3ad2c58f93d7a99ca0
Jatinder Kumar <email.jatinderkumar@gmail.com>
2025-01-13 22:55:35 +0530

    NextProject Code

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import HomePage from '../Features/HomePage/HomePage';
+export default function index() {
+  // return <HomePage />;
+  return <>
+  test
+  </>;
+}
+// export default function Home() {
+//   return <h1>Hello, Next.js!</h1>;
+// }
---
+++ src/pages/recoverPassword.js
@@ -0,0 +1,5 @@
+import RecoverPwd from '../features/RecoverPwd/RecoverPwd';
+
+export default function RecoverPassword() {
+  return <RecoverPwd />;
+}
